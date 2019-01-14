7d7180e5c29ea4664234ebefbb2dca164221752d
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-14 17:57:33 +0700

    implement printName

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Learn API");
+
+const listDocumentName = document.getElementById("name");
+
+const getDataAsync = async () => {
+  const response = await fetch("https://swapi.co/api/people");
+  const data = await response.json();
+  const result = data.results;
+
+  result.forEach( name => console.log(name.name))
+
+  const printName = result.forEach(character => {
+      listDocumentName.innerHTML += `<li>${character.name}</li>`;
+  });
+};
+
+getDataAsync();
