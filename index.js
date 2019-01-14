bb52f10b87771a66232e6cbbe2728f55bfec2131
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-14 19:24:39 +0700

    Implement <LI> to Pokemon section

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 console.log("Learn API");
 
-const listDocumentName = document.getElementById("name");
+const listStarWars = document.getElementById("nameStarWars");
+const listPokemon = document.getElementById("namePokemon");
 
-const getDataAsync = async () => {
+const getStarAsync = async () => {
   const response = await fetch("https://swapi.co/api/people");
   const data = await response.json();
   const result = data.results;
 
-  result.forEach( name => console.log(name.name))
+  result.forEach(name => console.log(name.name));
 
   const printName = result.forEach(character => {
-      listDocumentName.innerHTML += `<li>${character.name}</li>`;
+    listStarWars.innerHTML += `<li>${character.name}</li>`;
   });
 };
 
-getDataAsync();
+const getPokemonAsync = async () => {
+  const response = await fetch("https://api.pokemontcg.io/v1/cards", {
+    headers: {
+      Count: 1
+    }
+  });
+  const data = await response.json();
+  const result = data.cards.slice(0, 10);
+
+  result.forEach(name => console.log(name.name));
+
+  result.forEach(character => {
+    listPokemon.innerHTML += `<li>${character.name}</li>`;
+  });
+};
+
+getStarAsync();
+getPokemonAsync();
